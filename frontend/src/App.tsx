208e12fedc0510d5298006c3af719a780f0d054d
Erland Svingen <erland.svingen@fremtind.no>
2022-11-17 13:02:16 +0100

    test particle-api-js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-//import config from "./config"
-//config.particletoken
+import config from "./config"
+
+// https://docs.particle.io/reference/cloud-apis/javascript/
+import ParticleAPI from 'particle-api-js';
+const api = new ParticleAPI();
 
 function App() {
   const [count, setCount] = useState(0)
+
+  /*
+  const test = async () => {
+    const devices = await api.listDevices({ auth: config.particletoken });
+    console.error('MY DEVICES:', devices)
+  }
+  */
 
   return (
     <div className="App">
