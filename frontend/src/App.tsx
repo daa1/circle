2c73684fd62366aeb3b9c5d25ff9d3aa66329c6c
Karl August Korslund <karl.august.korslund@fremtind.no>
2022-11-17 13:41:19 +0100

    Removed particle api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,16 @@
-import './App.css'
-import config from "./config"
+import "./App.css";
+import config from "./config";
 
-// https://docs.particle.io/reference/cloud-apis/javascript/
-import ParticleAPI from 'particle-api-js';
-const api = new ParticleAPI();
 import NavTabs from "./NavTabs";
 
 function App() {
-
-  /*
-  const test = async () => {
-    const devices = await api.listDevices({ auth: config.particletoken });
-    console.error('MY DEVICES:', devices)
-  }
-  */
-
   return (
     <div className="App">
       <nav>
-          <NavTabs></NavTabs>
+        <NavTabs></NavTabs>
       </nav>
     </div>
   );
 }
 
-export default App
+export default App;
